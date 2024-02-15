00ea53aba0941c165a954a447ed768b86e7511a2
Matthew Pappas <117243210+matt-pappas-cf@users.noreply.github.com>
2024-02-15 13:47:32 -0800

    Update folders.tf

--- folders.tf
+++ folders.tf
@@ -7,21 +7,21 @@ data "google_folder" "aw_folder" {
 *************************************************/
 
 module "management_folder" {
-  source = "github.com/Coalfire-CF/terraform-gcp-folder"
+  source = "github.com/Coalfire-CF/terraform-google-folder"
 
   name   = "${var.folder_prefix}-management"
   parent = data.google_folder.aw_folder.name
 }
 
 module "networking_folder" {
-  source = "github.com/Coalfire-CF/terraform-gcp-folder"
+  source = "github.com/Coalfire-CF/terraform-google-folder"
 
   name   = "${var.folder_prefix}-networking"
   parent = data.google_folder.aw_folder.name
 }
 
 module "application_folder" {
-  source = "github.com/Coalfire-CF/terraform-gcp-folder"
+  source = "github.com/Coalfire-CF/terraform-google-folder"
 
   name   = "${var.folder_prefix}-application"
   parent = data.google_folder.aw_folder.name
