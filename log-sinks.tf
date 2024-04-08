945366f011583ad9e91130c7eeaf1e3a92a7620a
Michael Scribellito <mscribellito@gmail.com>
2024-04-08 09:24:49 -0400

    add variable to enable configurable sinks

--- log-sinks.tf
+++ log-sinks.tf
@@ -11,7 +11,9 @@ resource "random_string" "suffix_sink" {
 module "log_export" {
   source = "github.com/Coalfire-CF/terraform-google-log-export"
 
-  destination_uri = module.destination.destination_uri
+  count = var.create_log_sinks ? 1 : 0
+
+  destination_uri = module.destination[0].destination_uri
   filter          = var.log_filter
 
   log_sink_name          = "${var.sink_prefix}-org-${random_string.suffix_sink.result}"
@@ -26,10 +28,13 @@ module "log_export" {
 }
 
 module "destination" {
-  source                   = "github.com/Coalfire-CF/terraform-google-log-export/modules/pubsub"
+  source = "github.com/Coalfire-CF/terraform-google-log-export/modules/pubsub"
+
+  count = var.create_log_sinks ? 1 : 0
+
   project_id               = module.management_project.project_id
   topic_name               = "${var.topic_prefix}-org-logs-${random_string.suffix_sink.result}"
-  log_sink_writer_identity = module.log_export.writer_identity
+  log_sink_writer_identity = module.log_export[0].writer_identity
   create_subscriber        = true
   kms_key_name             = module.kms.keys["pub-sub"]
 
