f467339432ee86ed9939cbccc8510cd1438fdf5b
douglas-f <douglas-f@users.noreply.github.com>
2026-07-05 19:30:27 +0000

    chore: refresh dependabot.yml & TF sources

--- example/setup.tf
+++ example/setup.tf
@@ -1,5 +1,5 @@
 module "bootstrap" {
-    source = "github.com/Coalfire-CF/terraform-google-security-core?ref=v1.0.17"
+    source = "git::https://github.com/Coalfire-CF/terraform-google-security-core?ref=v1.0.17"
 
     org_id = var.org_id
     aw_folder_id = var.aw_folder_id
